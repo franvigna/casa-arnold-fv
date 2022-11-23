dcd464f59c1c8f36b39726bc55c85bb10b0fd50d
Cisco <vignardel.frnacisco@gmail.com>
2022-11-23 15:19:51 -0300

    programmer contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,15 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className='text-center py-3'>
-                        Copytight &copy; Casa Arnold 2023
+                        Copyright &copy; Casa Arnold 2023
                     </Col>
                 </Row>
+                <div className='text-center contact-programmer'>
+                    Contacto del programador:
+                    <a href="http://www.linkedin.com/in/franciscovignardel">
+                    <b> vignardel.francisco@gmail.com</b>
+                    </a>
+                </div>
             </Container>
         </footer>
     )
